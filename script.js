8388072480a68191778ab0af0e36df8efb7acd7e
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-07-24 22:59:43 +0300

    Add functionality to close btn of mobile menu

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const openMobile = document.getElementById('bars');
 const closeMobile = document.getElementById('close');
-const mobilemenu = document.getElementById('mobile_m')
+const mobilemenu = document.getElementById('mobile_m');
+const menuList = document.getElementsByClassName('navlist');
+console.log(menuList)
 
 function openMobileMenu() {
-  mobilemenu.style.display = 'flex'
+  mobilemenu.style.display = 'flex';
 }
-function closeMobile(){
-openMobile.addEventListener('click', openMobileMenu());
+
+function closeMobilemenu() {
+  mobilemenu.style.display = 'none';
 }
+for(let i = 0; i < menuList.length; i++){
+  menuList[i].addEventListener('click', closeMobilemenu);
+}
+openMobile.addEventListener('click', openMobileMenu);
+closeMobile.addEventListener('click', closeMobilemenu);
+

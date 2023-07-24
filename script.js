eaeb3880769d778a78f5c4fa7553922e066bb42a
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-07-25 00:58:33 +0300

    Rectify eslint errors

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const openMobile = document.getElementById('bars');
 const closeMobile = document.getElementById('close');
 const mobilemenu = document.getElementById('mobile_m');
 const menuList = document.getElementsByClassName('navlist');
-console.log(menuList)
+console.log(menuList);
 
 function openMobileMenu() {
   mobilemenu.style.display = 'flex';
@@ -11,9 +11,8 @@ function openMobileMenu() {
 function closeMobilemenu() {
   mobilemenu.style.display = 'none';
 }
-for(let i = 0; i < menuList.length; i++){
+for (let i = 0; i < menuList.length; i += 1) {
   menuList[i].addEventListener('click', closeMobilemenu);
 }
 openMobile.addEventListener('click', openMobileMenu);
 closeMobile.addEventListener('click', closeMobilemenu);
-

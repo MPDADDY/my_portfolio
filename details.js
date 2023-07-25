b505c122c1355b33e5998a74da114f462cae4a09
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-07-25 14:56:25 +0300

    Create work section dynamically using js

---
+++ details.js
@@ -0,0 +1,80 @@
+const workSection = document.getElementById('work_section');
+const Articles = [
+  {
+    articleClass: 'work',
+    mobileImg: 'images/Snapshoot Portfolio3.png',
+    desktopImg: 'images/d1.png',
+    heading: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    desktopClass: 'desktop',
+    reverseClass: 'reverse',
+    
+  },
+  {
+    articleClass: 'work reverse',
+    mobileImg: 'images/Snapshoot Portfolio3.png',
+    desktopImg: 'images/d2.png',
+    heading: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    desktopClass: 'desktop',
+    reverseClass: 'reverse',
+    
+  },
+  {
+    articleClass: 'work',
+    mobileImg: 'images/Snapshoot Portfolio3.png',
+    desktopImg: 'images/d3.png',
+    heading: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    desktopClass: 'desktop',
+    reverseClass: 'reverse',
+    
+  },
+  {
+    articleClass: 'work reverse',
+    mobileImg: 'images/Snapshoot Portfolio2.png',
+    desktopImg: 'images/desktop7.png',
+    heading: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    desktopClass: 'desktop',
+    reverseClass: 'reverse',
+    
+  }
+]
+
+for(let i = 0; i < Articles.length; i += 1) {
+  const myArticles = document.getElementById('work_section').innerHTML;
+  workSection.innerHTML =
+    `<article class="${Articles[i].articleClass}">
+                <div class="work_img">
+                    <img class="mobile" src="${Articles[i].mobileImg}" alt="work screenshot">
+                    <img class="desktop" src="${Articles[i].desktopImg}">
+                </div>
+                <div class="work_info">
+                    <h2>Tonic</h2>
+                    <div class="canopy">
+                        <div>
+                            <h5>CANOPY</h5>
+                        </div>
+                        <ul class="canopy_languages">
+                            <li>
+                                <img src="images/Counter.png" alt="counter image">
+                                <span>Backend dev</span>
+                            </li>
+                            <li>
+                                <img src="images/Counter.png" alt="counter image">
+                                <span>2015</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <p>A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
+                    <ul class="languages">
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                    </ul>
+                    <button type="button">See Project</button>
+                </div>
+
+            </article>${myArticles}`;
+}

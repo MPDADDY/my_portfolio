5c612b140df365bf30b2e94c93fa339a57cfa47d
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-07-27 17:59:28 +0300

    Add locale storage Object and variables

---
+++ localeStorage.js
@@ -0,0 +1,24 @@
+const userName = document.getElementById('name');
+const email = document.getElementById('email');
+const userMessage = document.getElementById('message');
+const form = document.getElementById('contact_form');
+
+const formInputsData = {
+  name: '',
+  email: '',
+  message: '',
+};
+form.addEventListener('input', () => {
+  formInputsData.name = userName.value;
+  formInputsData.email = email.value;
+  formInputsData.message = userMessage.value;
+  localStorage.setItem('formInputsData', JSON.stringify(formInputsData));
+});
+window.onload = () => {
+  const data = JSON.parse(localStorage.getItem('formInputsData'));
+  if (data) {
+    userName.value = data.name;
+    email.value = data.email;
+    userMessage.value = data.message;
+  }
+};
